dce96cf92426201bfd94da659debed1adb295dfe
FriquetLuca <friquet.luca@gmail.com>
2026-09-13 19:08:07 +0200

    put the https redirect into it's own middleware

---
+++ actix_server/src/server/https_redirect.rs
@@ -0,0 +1,93 @@
+use actix_web::body::{BoxBody, MessageBody};
+use actix_web::{
+  dev::{forward_ready, Service, ServiceRequest, ServiceResponse, Transform},
+  http, Error, HttpResponse,
+};
+use futures_util::future::LocalBoxFuture;
+use std::future::{ready, Ready};
+
+use crate::server::is_secure_request::is_secure_request;
+
+pub struct HttpsRedirect {
+  public_host_name: String,
+  public_host_port: u16,
+}
+
+impl HttpsRedirect {
+  pub fn new(public_host_name: String, public_host_port: u16) -> Self {
+    Self {
+      public_host_name,
+      public_host_port,
+    }
+  }
+}
+
+impl<S, B> Transform<S, ServiceRequest> for HttpsRedirect
+where
+  S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
+  S::Future: 'static,
+  B: MessageBody + 'static,
+{
+  type Response = ServiceResponse<BoxBody>;
+  type Error = Error;
+  type InitError = ();
+  type Transform = HttpsRedirectMiddleware<S>;
+  type Future = Ready<Result<Self::Transform, Self::InitError>>;
+
+  fn new_transform(&self, service: S) -> Self::Future {
+    ready(Ok(HttpsRedirectMiddleware {
+      service,
+      public_host_name: self.public_host_name.clone(),
+      public_host_port: self.public_host_port,
+    }))
+  }
+}
+
+pub struct HttpsRedirectMiddleware<S> {
+  service: S,
+  public_host_name: String,
+  public_host_port: u16,
+}
+
+impl<S, B> Service<ServiceRequest> for HttpsRedirectMiddleware<S>
+where
+  S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
+  S::Future: 'static,
+  B: MessageBody + 'static,
+{
+  type Response = ServiceResponse<BoxBody>;
+  type Error = Error;
+  type Future = LocalBoxFuture<'static, Result<Self::Response, Self::Error>>;
+
+  forward_ready!(service);
+
+  fn call(&self, req: ServiceRequest) -> Self::Future {
+    if is_secure_request(&req) {
+      let fut = self.service.call(req);
+      return Box::pin(async move {
+        let res = fut.await?;
+        Ok(res.map_into_boxed_body())
+      });
+    }
+
+    let uri = req.uri().to_owned();
+    let host_only = self.public_host_name.clone();
+    let public_port = self.public_host_port;
+
+    let url = if public_port == 443 {
+      format!("https://{host_only}{uri}")
+    } else {
+      format!("https://{host_only}:{public_port}{uri}")
+    };
+
+    Box::pin(async move {
+      Ok(
+        req.into_response(
+          HttpResponse::MovedPermanently()
+            .append_header((http::header::LOCATION, url))
+            .finish(),
+        ),
+      )
+    })
+  }
+}
--- actix_server/src/server/mod.rs
+++ actix_server/src/server/mod.rs
@@ -4,22 +4,19 @@ mod csr;
 mod error;
 #[cfg(feature = "proxy_default_service")]
 mod forward;
+mod https_redirect;
 mod is_secure_request;
 
 use crate::config::env_config;
 use crate::server::configure_default_services::configure_default_services;
-use crate::server::is_secure_request::is_secure_request;
+use crate::server::https_redirect::HttpsRedirect;
 use crate::states::SharedBloom;
 use actix_web::body::MessageBody;
-use actix_web::dev::{
-  Service, ServiceFactory, ServiceRequest, ServiceResponse,
-};
-use actix_web::http;
+use actix_web::dev::{ServiceFactory, ServiceRequest, ServiceResponse};
 use actix_web::middleware::Logger;
 use actix_web::web::Data;
 use actix_web::{web, App, HttpResponse};
 use error::AppError;
-use futures_util::future::{self, Either, FutureExt};
 use sqlx::{Pool, Postgres};
 
 pub(crate) fn create_app(
@@ -66,29 +63,10 @@ pub(crate) fn create_app(
     });
   let trusted_proxies = env_config().trusted_proxies.clone();
   App::new()
-    .wrap_fn(|sreq, srv| {
-      if is_secure_request(&sreq) {
-        return Either::Left(srv.call(sreq).map(|res| res));
-      }
-
-      let uri = sreq.uri().to_owned();
-      let host_only = &env_config().public_host_name;
-      let public_port = env_config().public_host_port;
-
-      let url = if public_port == 443 {
-        format!("https://{host_only}{uri}")
-      } else {
-        format!("https://{host_only}:{public_port}{uri}")
-      };
-
-      Either::Right(future::ready(Ok(
-        sreq.into_response(
-          HttpResponse::MovedPermanently()
-            .append_header((http::header::LOCATION, url))
-            .finish(),
-        ),
-      )))
-    })
+    .wrap(HttpsRedirect::new(
+      env_config().public_host_name.clone(),
+      env_config().public_host_port,
+    ))
     .wrap(Logger::default())
     .wrap(crate::middleware::create_session_middleware())
     .app_data(json_config)
